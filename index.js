6c19848ae412c8642b752fc7465c61340c0e2034
sarojinigarapati <sarojinigarapati@gmail.com>
2016-08-28 17:55:15 -0700

    clean up after validation

--- index.js
+++ index.js
@@ -20,28 +20,6 @@
  */
 var APP_ID = 'amzn1.ask.skill.e073d6df-b71d-4d36-90e6-fa696701a730';
 
-/**
- * Array containing knock knock jokes.
- */
-var JOKE_LIST = [
-    {setup: "To", speechPunchline: "Correct grammar is <break time=\"0.2s\" /> to whom.",
-        cardPunchline: "Correct grammar is 'to whom'."},
-    {setup: "Beets!", speechPunchline: "Beats me!", cardPunchline: "Beats me!"},
-    {setup: "Little Old Lady", speechPunchline: "I didn't know you could yodel!",
-        cardPunchline: "I didn't know you could yodel!"},
-    {setup: "A broken pencil", speechPunchline: "Never mind. It's pointless.",
-        cardPunchline: "Never mind. It's pointless."},
-    {setup: "Snow", speechPunchline: "Snow use. I forgot", cardPunchline: "Snow use. I forgot"},
-    {setup: "Boo", speechPunchline: "Aww <break time=\"0.3s\" /> it's okay <break time=\"0.3s\" /> don't cry.",
-        cardPunchline: "Aww, it's okay, don't cry."},
-    {setup: "Woo", speechPunchline: "Don't get so excited, it's just a joke",
-        cardPunchline: "Don't get so excited, it's just a joke"},
-    {setup: "Spell", speechPunchline: "<say-as interpret-as=\"characters\">who</say-as>",
-        cardPunchline: "w.h.o"},
-    {setup: "Atch", speechPunchline: "I didn't know you had a cold!", cardPunchline: "I didn't know you had a cold!"},
-    {setup: "Owls", speechPunchline: "Yes, they do.", cardPunchline: "Yes, they do."},
-    {setup: "Berry!", speechPunchline: "Berry nice to meet you.", cardPunchline: "Berry nice to meet you."}
-];
 var WORD_LIST = ["chair", "table", "pen", "computer", "pencil", "eraser", 
                  "fan", "light", "bed","stove","car","cup","toothbrush",
                  "guitar","phone","book","aeroplane","bottle","shirt","pant"];
@@ -103,13 +81,6 @@ RememberMeSkill.prototype.intentHandlers = {
     },
     "WordRecognitionIntent": function (intent, session, response) {
     	handleUserInputForWordRecognition(intent, session, response);
-    },
-    "WhosThereIntent": function (intent, session, response) {
-        handleWhosThereIntent(session, response);
-    },
-
-    "SetupNameWhoIntent": function (intent, session, response) {
-        handleSetupNameWhoIntent(session, response);
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
@@ -353,158 +324,9 @@ function handleUserInputForWordRecognition(intent, session, response) {
     response.askWithCard(speechOutput, repromptOutput, "Remember Me", speechText);
 }
 
-/**
- * Selects a joke randomly and starts it off by saying "Knock knock".
- */
-function _handleLaunchRequest(session, response) {
-    var speechText = "";
-
-    //Reprompt speech will be triggered if the user doesn't respond.
-    var repromptText = "You can say any number between 1 and 10";
-
-    //Check if session variables are already initialized.
-    if (session.attributes.stage) {
-
-        //Ensure the dialogue is on the correct stage.
-        if (session.attributes.stage === 0) {
-            //The user is trying to say the number of words
-            speechText = "knock knock!";
-        } else {
-            //The user attempted to jump to the intent of another stage.
-            session.attributes.stage = 0;
-            speechText = "That's not how knock knock jokes work! "
-                + "knock knock";
-        }
-    } else {
-        //Select a random joke and store it in the session variables.
-        var jokeID = Math.floor(Math.random() * JOKE_LIST.length);
-
-        //The stage variable tracks the phase of the dialogue. 
-        //When this function completes, it will be on stage 1.
-        session.attributes.stage = 1;
-        session.attributes.setup = JOKE_LIST[jokeID].setup;
-        session.attributes.speechPunchline = JOKE_LIST[jokeID].speechPunchline;
-        session.attributes.cardPunchline = JOKE_LIST[jokeID].cardPunchline;
-
-        speechText = "Knock knock!";
-    }
-
-    var speechOutput = {
-        speech: speechText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
-    };
-    var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
-    };
-//    response.askWithCard(speechOutput, repromptOutput, "Wise Guy", speechText);
-}
-
-/**
- * Responds to the user saying "Who's there".
- */
-function handleWhosThereIntent(session, response) {
-    var speechText = "";
-    var repromptText = "";
-
-    if (session.attributes.stage) {
-        if (session.attributes.stage === 1) {
-            //Retrieve the joke's setup text.
-            speechText = session.attributes.setup;
-
-            //Advance the stage of the dialogue.
-            session.attributes.stage = 2;
-
-            repromptText = "You can ask, " + speechText + " who?";
-        } else {
-            session.attributes.stage = 1;
-            speechText = "That's not how knock knock jokes work! <break time=\"0.3s\" /> "
-                + "knock knock";
-
-            repromptText = "You can ask, who's there."
-        }
-    } else {
-
-        //If the session attributes are not found, the joke must restart. 
-        speechText = "Sorry, I couldn't correctly retrieve the joke. "
-            + "You can say, tell me a joke";
-
-        repromptText = "You can say, tell me a joke";
-    }
-
-    var speechOutput = {
-        speech: '<speak>' + speechText + '</speak>',
-        type: AlexaSkill.speechOutputType.SSML
-    };
-    var repromptOutput = {
-        speech: '<speak>' + repromptText + '</speak>',
-        type: AlexaSkill.speechOutputType.SSML
-    };
-    response.ask(speechOutput, repromptOutput);
-}
-
-/**
- * Delivers the punchline of the joke after the user responds to the setup.
- */
-function handleSetupNameWhoIntent(session, response) {
-    var speechText = "",
-        repromptText = "",
-        speechOutput,
-        repromptOutput,
-        cardOutput;
-
-    if (session.attributes.stage) {
-        if (session.attributes.stage === 2) {
-            speechText = session.attributes.speechPunchline;
-            cardOutput = session.attributes.cardPunchline;
-            speechOutput = {
-                speech: '<speak>' + speechText + '</speak>',
-                type: AlexaSkill.speechOutputType.SSML
-            };
-            //If the joke completes successfully, this function uses a "tell" response.
-            response.tellWithCard(speechOutput, "Wise Guy", cardOutput);
-        } else {
-
-            session.attributes.stage = 1;
-            speechText = "That's not how knock knock jokes work! <break time=\"0.3s\" /> "
-                + "Knock knock!";
-            cardOutput = "That's not how knock knock jokes work! "
-                + "Knock knock!";
-
-            repromptText = "You can ask who's there.";
-
-            speechOutput = {
-                speech: speechText,
-                type: AlexaSkill.speechOutputType.SSML
-            };
-            repromptOutput = {
-                speech: repromptText,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            //If the joke has to be restarted, this function uses an "ask" response.
-            response.askWithCard(speechOutput, repromptOutput, "Wise Guy", cardOutput);
-        }
-    } else {
-        speechText = "Sorry, I couldn't correctly retrieve the joke. "
-            + "You can say, tell me a joke";
-
-        repromptText = "You can say, tell me a joke";
-
-        speechOutput = {
-            speech: speechText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        repromptOutput = {
-            speech: repromptText,
-            type: AlexaSkill.speechOutputType.PLAIN_TEXT
-        };
-        response.askWithCard(speechOutput, repromptOutput, "Wise Guy", speechOutput);
-    }
-}
-
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the WiseGuy Skill.
+    // Create an instance of the Remember Me Skill.
     var skill = new RememberMeSkill();
     skill.execute(event, context);
 };
